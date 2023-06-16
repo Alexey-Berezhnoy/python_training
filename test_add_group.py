c18668f4455ea039612b7b9f75b1807d2d942df0
Alexey-Berezhnoy <alberezhnoytraining@gmail.com>
2023-06-16 19:57:32 +0300

    В репозиторий добавлен слегка отредактированный файл рекордера.Теперь под файрфоксом.

--- test_add_group.py
+++ test_add_group.py
@@ -9,12 +9,12 @@ import unittest, time, re
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -22,20 +22,20 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("222222222")
+        wd.find_element_by_name("group_name").send_keys("03")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("2sfgdfgd")
+        wd.find_element_by_name("group_header").send_keys("fgdfhgd")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("2fvbf45bfg676")
+        wd.find_element_by_name("group_footer").send_keys("dgnfdg")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
-        #ERROR: Caught exception [unknown command []]
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
